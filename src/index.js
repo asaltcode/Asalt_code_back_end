770b660c5ec870c8487e4b98693b5abccd008aa7
asaltcode <elangovanr558@gmail.com>
2024-03-06 10:07:28 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -10,32 +10,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
-
-app.post('/ref', (req, res)=>{
-  const refreshToken = req.cookies['refreshToken'];
-  if (!refreshToken) {
-    return res.status(401).send('Access Denied. No refresh token provided.');
-  }
-
-  try {
-    const decoded = jwt.verify(refreshToken, 'ldjfuLKjhusdWEfiyhxcvkLKyh');
-    const accessToken = jwt.sign({ user: decoded.user }, 'ldjfuLKjhusdWEfiyhxcvkLKyh', { expiresIn: '1h' });
-    res
-      .header('Authorization', accessToken)
-      .send(decoded.user);
-  } catch (error) {
-    return res.status(400).send('Invalid refresh token.');
-  }
-})
+app.use(AppRoutes)
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong' });
 });
 
-app.use(AppRoutes)
 
 app.listen(PORT, () =>
-  console.log(`Server listeing at http://localhost:${PORT}`)
+  console.log(`Server listeing at ${PORT}`)
 );
