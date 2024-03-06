65eedf6d5a11c9ee1bfef8d24181367ce97528fc
asaltcode <elangovanr558@gmail.com>
2024-03-06 10:20:17 +0530

    file director change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import AppRoutes from "./routes/index.js";
+import AppRoutes from "./src/routes/index.js";
 import cookieParser from 'cookie-parser'
 dotenv.config();
 
